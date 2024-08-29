70a3d1438a6926f2776330fb6acc297ddd3dea4d
wgbbiao <wgb237@163.com>
2024-08-29 16:49:23 +0800

    init

---
+++ src/controller/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\plug\controller;
+
+
+class Auth
+{
+    function signature(): string
+    {
+        $app_id = 'your app_id';
+        $app_secret =  'your app_secret';
+        // 随机字符串
+        $nonceStr = random_bytes(20);
+        $timestamp = time();
+        $signStr = $app_id . $app_secret . $nonceStr . $timestamp;
+        return sha1($signStr);
+    }
+}
